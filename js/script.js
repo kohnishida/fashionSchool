c8915df8db6d065115540507fd3be3a94db8c4b5
kohnishida <86033362+kohnishida@users.noreply.github.com>
2021-06-17 12:09:07 +0900

    Luxy.jsというライブラリを使ってみました。

---
+++ js/script.js
@@ -0,0 +1,89 @@
+//任意のタブにURLからリンクするための設定
+function GethashID (hashIDName){
+	if(hashIDName){
+		//タブ設定
+		$('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
+			var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
+			if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+				var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+				$('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+				$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+				//表示させるエリア設定
+				$(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+				$(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加	
+			}
+		});
+	}
+}
+
+//タブをクリックしたら
+$('.tab a').on('click', function() {
+	var idName = $(this).attr('href'); //タブ内のリンク名を取得	
+	GethashID (idName);//設定したタブの読み込みと
+	return false;//aタグを無効にする
+});
+
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+    $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+	GethashID (hashName);//設定したタブの読み込み
+});
+
+if(matchMedia('(min-width: 768px)').matches){
+    luxy.init();
+    } 
+
+
+
+
+
+// ランダム文字
+var Obj = {
+	loop: false,
+	minDisplayTime: 2000,// アニメーションの間隔時間
+	initialDelay: 200, // アニメーション開始までの遅延時間
+	autoStart: true,
+	in: {
+		effect: 'fadeInUp',//animate.css の中にある採用したい動きのクラス名
+		delayScale: 1,// 遅延時間の指数
+		delay: 100,// 文字ごとの遅延時間
+		sync: false,// アニメーションをすべての文字に同時適用するかどうか
+		shuffle: true,// 文字表示がランダムな順に表示されるかどうか
+	},
+	out: {// 終了時のアニメーション設定をしたい場合はここに追記
+	}
+}
+var element
+//初期設定
+function RandomInit() {
+	element= $(".randomAnime");
+	$(element[0]).textillate(Obj);
+}
+
+function RandomAnimeControl() {
+		var elemPos = $(element[1]).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+
+		if (scroll >= elemPos - windowHeight) {
+			$(element[1]).textillate(Obj);
+		}
+}
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	RandomInit(); /*初期設定を読み込み*/
+	RandomAnimeControl();/*アニメーション用の関数を呼ぶ*/
+});//ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+// ハンバーガーメニュー
+$('.menubtn').click(function(){
+	$('nav').toggleClass('navActive');
+});
+$('.w1000 a').click(function(){
+	$('nav').removeClass('navActive');
+});
